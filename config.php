7ddb6f755dc24f871f8666b4f846343c16594b93
FTeichmann <Franz.Teichmann@gmx.de>
2014-09-09 14:24:28 +0200

    todo: Content Negotiation

---
+++ config.php
@@ -0,0 +1,51 @@
+﻿<!DOCTYPE HTML>
+<?php
+//definierte Endpoints:
+$endpoints['lobit'] = "http://lobid.org/sparql/";
+$endpoints['europeana'] = "http://europeana.ontotext.com/sparql/";
+?>
+<html>
+<head>
+<style type="text/css">
+.codeblock {
+    border: 1px dashed gray;
+    background: #F0F0F0;
+    padding: 10px;
+}
+.codeblock script {
+    display: block;
+}
+</style>
+</head>
+<body id="top">
+<?php
+if(!$_POST){
+?>
+<form action="" method="post">
+<p>Welche Endpoints sollen einbezogen werden?</p><br>
+<?php 
+    //print all defined endpoints as checkboxes
+    foreach($endpoints as $endpoint){
+        echo "<input type=\"checkbox\" name=\"endpoint[]\" value=\"".$endpoint."\">".$endpoint."<br>\n";
+    }
+?>
+
+ <p><input type="submit" value="submit" /></p>
+</form>
+<?php
+    }else{
+        //write the config file for FedX
+        $config = fopen("ubleipzig_config.ttl","w");
+        fwrite($config, "@prefix fluid: <http://fluidops.org/config#>.\n\n");
+        if(!empty($_POST['endpoint'])){
+            foreach($_POST['endpoint'] as $endpoint){
+                fwrite($config, "<http://".array_search($endpoint, $endpoints)."> fluid:store \"SPARQLEndpoint\";\n");
+                fwrite($config, "fluid:SPARQLEndpoint \"".$endpoint."\".\n\n");
+            }
+        fclose($config);
+        echo "<p>Konfiguration abgeschlossen.</p>";
+        }
+    }
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,43 +1,27 @@
-﻿<!DOCTYPE HTML>
 <?php
-//definierte Endpoints:
-$endpoints['lobit'] = "http://lobid.org/sparql/";
-$endpoints['europeana'] = "http://europeana.ontotext.com/sparql/";
+//Post oder Get nicht gesetzt? -> Ausgabe des Formulars
+if(!($_POST or $_GET)){
 ?>
-
+<!DOCTYPE HTML>
 <html>
 <head>
 <style type="text/css">
 .codeblock {
-	border: 1px dashed gray;
-	background: #F0F0F0;
-	padding: 10px;
+    border: 1px dashed gray;
+    background: #F0F0F0;
+    padding: 10px;
 }
-.codeblock script {
-	display: block;
+codeblock script {
+    display: block;
 }
 </style>
 </head>
 <body id="top">
 
-<form action="index.php" method="post">
-<p>Welche Endpoints sollen einbezogen werden?</p><br>
-
-<?php 
-    //print all defined endpoints as checkboxes
-    foreach($endpoints as $endpoint){
-        echo "<input type=\"checkbox\" name=\"endpoint[]\" value=\"".$endpoint."\">".$endpoint."<br>\n";
-    }
-?>
-
+<form action="" method="post">
+<p>Bitte Sparql Query definieren.</p><br>
 <textarea name="query" cols="100" rows="10">
-<?php 
-		if (isset($_POST['query'])){
-			echo $_POST['query'];
-		}else{
-			echo "select distinct ?Concept where {[] a ?Concept} LIMIT 100";
-		};
-?>
+select distinct ?Concept where {[] a ?Concept} LIMIT 100
 </textarea>
  <p><input type="submit" value="submit" /></p>
 </form>
@@ -45,23 +29,17 @@ $endpoints['europeana'] = "http://europeana.ontotext.com/sparql/";
 </html>
 
 <?php
-    if($_POST){
-    $query = $_POST['query'];
+}else{
+    //Verarbeitung der Query
+    //1. Schreiben in Queryfile für FedX
+    $query = ($_POST)? $_POST['query'] : $_GET['query'];
     $queryfile = fopen("query.txt","w");
     fwrite($queryfile,$query);
     fclose($queryfile);
-    //write the config file for FedX
-    $config = fopen("ubleipzig_config.ttl","w");
-    fwrite($config, "@prefix fluid: <http://fluidops.org/config#>.\n\n");
-    if(!empty($_POST['endpoint'])){
-        foreach($_POST['endpoint'] as $endpoint){
-            fwrite($config, "<http://".array_search($endpoint, $endpoints)."> fluid:store \"SPARQLEndpoint\";\n");
-            fwrite($config, "fluid:SPARQLEndpoint \"".$endpoint."\".\n\n");
-        }
-    fclose($config);
-    }
-    
+    //Aufruf von FedX
     $FedX_response = shell_exec("cd ./FedX && ./cli.sh -d ./../ubleipzig_config.ttl @q ./../query.txt");
+    
+    //todo: Content Negotiation
     echo "<p>Antwort des Federation Dispatchers:</p></br>";
     print($FedX_response);
 }
