ad9a327a2d103d32927681e7818ed8fc5d97a055
FTeichmann <Franz.Teichmann@gmx.de>
2014-09-10 10:04:19 +0200

    initiated content negotiation, error persisting data after 39 entries remains, correct execution of FedX remains unclear, FedX response is no RDF

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//Post oder Get nicht gesetzt? -> Ausgabe des Formulars
+//Post oder Get not set print formular
 if(!($_POST or $_GET)){
 ?>
 <!DOCTYPE HTML>
@@ -19,7 +19,7 @@ codeblock script {
 <body id="top">
 
 <form action="" method="post">
-<p>Bitte Sparql Query definieren.</p><br>
+<p>Bitte Sparql Query definieren:</p><br>
 <textarea name="query" cols="100" rows="10">
 select distinct ?Concept where {[] a ?Concept} LIMIT 100
 </textarea>
@@ -30,18 +30,29 @@ select distinct ?Concept where {[] a ?Concept} LIMIT 100
 
 <?php
 }else{
-    //Verarbeitung der Query
-    //1. Schreiben in Queryfile für FedX
+    //processing the query
+    //writing queryfile for FedX
     $query = ($_POST)? $_POST['query'] : $_GET['query'];
     $queryfile = fopen("query.txt","w");
     fwrite($queryfile,$query);
     fclose($queryfile);
     //Aufruf von FedX
     $FedX_response = shell_exec("cd ./FedX && ./cli.sh -d ./../ubleipzig_config.ttl @q ./../query.txt");
-    
-    //todo: Content Negotiation
     echo "<p>Antwort des Federation Dispatchers:</p></br>";
     print($FedX_response);
+    
+    if(stristr($_SERVER['HTTP_ACCEPT'], "application/xhtml+xml")){
+        //header("Content-Type: application/xhtml+xml; charset=utf-8");
+        //Format: xhtml+xml Seite
+    }elseif(stristr($_SERVER['HTTP_ACCEPT'], "application/rdf+xml")){
+        //header("Content-Type: text/html; charset=utf-8");
+        //Format: RDF/XML
+    }elseif(stristr($_SERVER['HTTP_ACCEPT'], "text/turtle")){
+        //Format: Turtle
+        
+    }else{
+        //user prob. has internet explorer?
+    }
 }
 
 ?>
